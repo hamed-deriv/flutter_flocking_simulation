7db4572d4d2a73d772f0b0204d1a70f0f7ae0e81
Hamed Rezaee <57184669+hamed-deriv@users.noreply.github.com>
2023-09-15 19:27:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_flocking_simulation
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_flocking_simulation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_flocking_simulation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
